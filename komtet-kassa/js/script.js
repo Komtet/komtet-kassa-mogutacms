cb124fede99b2d683c719587f34ba2d68a489f41
Юрий Никитин <yura.motmom@gmail.com>
2021-03-22 08:32:05 +0300

    правки по ревью

--- komtet-kassa/js/script.js
+++ komtet-kassa/js/script.js
@@ -4,42 +4,42 @@
 var komtetKassaModule = (function() {
 
   return {
-    pluginName: "komtet-kassa", //название плагина
+    pluginName: 'komtet-kassa', //название плагина
 
     init: function() {
       // Сохраняет базовые настроки
 
 	  $('body').on('click', '.section-komtet-kassa .base-setting-save', function() {
 
-        let prepaymentStatus = $(".base-settings-open select[name=prepayment_check_status]").val();
-        let is_prepayment_check = prepaymentStatus != "false"
+        let prepaymentStatus = $('.base-settings-open select[name=prepayment_check_status]').val();
+        let is_prepayment_check = prepaymentStatus != 'false'
 
         $.ajax({
-          type: "POST",
-          url: mgBaseDir+"/ajaxrequest",
+          type: 'POST',
+          url: mgBaseDir+'/ajaxrequest',
           data: {
             pluginHandler: 'komtet-kassa', // имя папки в которой лежит данный плагин
-            actionerClass: "Pactioner",
-            action: "saveBaseOption", // название действия в классе
+            actionerClass: 'Pactioner',
+            action: 'saveBaseOption', // название действия в классе
             data:{
-              shop_id : $(".base-settings-open input[name=shop_id]").val(),
-              secret : $(".base-settings-open input[name=secret]").val(),
-              queue_id : $(".base-settings-open input[name=queue_id]").val(),
-              sno : $(".base-settings-open select[name=sno]").val(),
-              is_print : $(".base-settings-open input[name=is_print]").prop('checked'),
+              shop_id : $('.base-settings-open input[name=shop_id]').val(),
+              secret : $('.base-settings-open input[name=secret]').val(),
+              queue_id : $('.base-settings-open input[name=queue_id]').val(),
+              sno : $('.base-settings-open select[name=sno]').val(),
+              is_print : $('.base-settings-open input[name=is_print]').prop('checked'),
               payments: komtetKassaModule.listOfPayments,
 
               prepayment_check_status : prepaymentStatus,
-              fullpayment_check_status : $(".base-settings-open select[name=fullpayment_check_status]").val(),
+              fullpayment_check_status : $('.base-settings-open select[name=fullpayment_check_status]').val(),
               is_prepayment_check: is_prepayment_check,
             },
           },
-          dataType: "json",
+          dataType: 'json',
           success: function(response) {
             // Выводим нотификацию с результатом (ошибка или успех)
             admin.indication(response.status, response.msg);
             // Если успех, обновляем страницу настроек плагина
-            if (response.status == "success") {
+            if (response.status == 'success') {
               admin.refreshPanel()
             }
           }
